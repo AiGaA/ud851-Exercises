9723ad8f14ed44ff62883e9efa8d168b617bcf41
AiGa <aiga.andrejeva@gmail.com>
2017-12-05 19:14:31 +0000

    add constructor NumberViewHolder within inner class NumberViewHolder in GreenAdapter

--- Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
+++ Lesson03-Green-Recycler-View/T03.02-Exercise-ViewHolder/app/src/main/java/com/example/android/recyclerview/GreenAdapter.java
@@ -121,10 +121,16 @@ public class GreenAdapter extends RecyclerView.Adapter<GreenAdapter.NumberViewHo
     class NumberViewHolder extends RecyclerView.ViewHolder{
         // COMPLETED (13) Within NumberViewHolder, create a TextView variable called listItemNumberView
         TextView listItemNumberView;
+
+        // COMPLETED (14) Create a constructor for NumberViewHolder that accepts a View called itemView as a parameter
+        // COMPLETED (15) Within the constructor, call super(itemView) and then find listItemNumberView by ID
+        public NumberViewHolder(View itemView){
+            super (itemView);
+            listItemNumberView = (TextView) itemView.findViewById(R.id.tv_item_number);
+        }
     }
 
-    // TODO (14) Create a constructor for NumberViewHolder that accepts a View called itemView as a parameter
-    // TODO (15) Within the constructor, call super(itemView) and then find listItemNumberView by ID
+
 
     // TODO (16) Within the NumberViewHolder class, create a void method called bind that accepts an int parameter called listIndex
     // TODO (17) Within bind, set the text of listItemNumberView to the listIndex
